482958fa65438134a72a953569c6e7a83c2f9550
Shavidini Ekanayake <77123486+ShavidiniEkanayake@users.noreply.github.com>
2023-02-21 12:27:05 +0530

    initilize projct

---
+++ src/main/java/com/rootcode/practicalbe/PracticalBeApplication.java
@@ -0,0 +1,13 @@
+package com.rootcode.practicalbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticalBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PracticalBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rootcode/practicalbe/PracticalBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rootcode.practicalbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticalBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
